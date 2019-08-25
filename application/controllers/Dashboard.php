d9c50322e810439daf6010759b8e1440bf38e382
Can Okay <canokay2014@gmail.com>
2019-08-25 13:00:13 +0300

    Base project init.

---
+++ application/controllers/Dashboard.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Dashboard extends CI_Controller {
+
+	public $project = "";
+	public $category = "";
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->project = "dashboard";
+		$this->category = "homepage";
+	}
+
+
+	public function homepage()
+	{
+		$context=array(
+			"title"					=>	"Welcome to CodeIgniter",
+			"project" 				=> 	$this->project,
+			"category" 				=>	$this->category,
+			"view" 					=>  $this->router->fetch_method(),
+		);
+		$this->load->view("$this->project/base",$context);
+	}
+}
