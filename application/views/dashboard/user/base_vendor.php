5ff01b10ca5db01c15ab037b4b5a40613fb783e6
Can Okay <canokay2014@gmail.com>
2019-08-27 01:22:01 +0300

    User View cdn link added.

--- application/views/dashboard/user/base_vendor.php
+++ application/views/dashboard/user/base_vendor.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 
+
 <head>
 
   <meta charset="utf-8">
@@ -20,11 +21,11 @@
 	?>
 
   <!-- Custom fonts for this template-->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="<?php echo base_url("assets/dashboard/")?>vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="<?php echo base_url("assets/dashboard/")?>css/sb-admin-2.css" rel="stylesheet">
+  <link href="<?php echo base_url("assets/dashboard/")?>css/sb-admin-2.min.css" rel="stylesheet">
 
 	<?php
 		if (isset($view_header_include)) {
@@ -39,15 +40,17 @@
 
 <?php $this->load->view("$project/$category/$view")   ?>
 
+
   <!-- Bootstrap core JavaScript-->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.bundle.js"></script>
+  <script src="<?php echo base_url("assets/dashboard/")?>vendor/jquery/jquery.min.js"></script>
+  <script src="<?php echo base_url("assets/dashboard/")?>vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+  <script src="<?php echo base_url("assets/dashboard/")?>vendor/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
   <script src="<?php echo base_url("assets/dashboard/")?>js/sb-admin-2.min.js"></script>
+
 	<?php
 			if (isset($view_footer_include)) {
 				echo "<!-- Page JS -->";
