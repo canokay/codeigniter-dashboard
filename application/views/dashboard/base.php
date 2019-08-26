46c71f40af7f44fc8ec6db038d78f3e2489a7da5
Can Okay <canokay2014@gmail.com>
2019-08-27 00:41:17 +0300

    theme min.css deleted.

--- application/views/dashboard/base.php
+++ application/views/dashboard/base.php
@@ -24,7 +24,7 @@
   <link href="<?php echo base_url("assets/dashboard/")?>https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="<?php echo base_url("assets/dashboard/")?>css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="<?php echo base_url("assets/dashboard/")?>css/sb-admin-2.css" rel="stylesheet">
 
 	<?php
 		if (isset($view_header_include)) {
