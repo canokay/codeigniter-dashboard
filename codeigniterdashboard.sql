697325c65b7e139aa3faf13a27660d3d49f2a64a
Can Okay <canokay2014@gmail.com>
2020-03-26 23:09:27 +0300

    1.1.10 - Update codeigniterdashboard.sql

--- codeigniterdashboard.sql
+++ codeigniterdashboard.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 27, 2019 at 02:08 AM
--- Server version: 10.1.37-MariaDB
--- PHP Version: 7.2.12
+-- Generation Time: Mar 26, 2020 at 08:18 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -32,10 +32,38 @@ CREATE TABLE `pages` (
   `id` int(11) NOT NULL,
   `url` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
   `title` varchar(255) COLLATE utf8_turkish_ci DEFAULT NULL,
-  `description` text COLLATE utf8_turkish_ci,
-  `isActive` tinyint(4) DEFAULT NULL,
-  `createdAt` datetime DEFAULT NULL
+  `description` text COLLATE utf8_turkish_ci DEFAULT NULL,
+  `is_active` tinyint(4) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `superuser`
+--
+
+CREATE TABLE `superuser` (
+  `id` int(11) NOT NULL,
+  `password` varchar(128) COLLATE utf8_turkish_ci NOT NULL,
+  `last_login` datetime(6) DEFAULT NULL,
+  `is_superuser` tinyint(1) NOT NULL,
+  `username` varchar(150) COLLATE utf8_turkish_ci NOT NULL,
+  `first_name` varchar(30) COLLATE utf8_turkish_ci NOT NULL,
+  `last_name` varchar(150) COLLATE utf8_turkish_ci NOT NULL,
+  `email` varchar(254) COLLATE utf8_turkish_ci NOT NULL,
+  `is_staff` tinyint(1) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `date_joined` datetime(6) NOT NULL,
+  `profile_photo` varchar(100) COLLATE utf8_turkish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_turkish_ci;
+
+--
+-- Dumping data for table `superuser`
+--
+
+INSERT INTO `superuser` (`id`, `password`, `last_login`, `is_superuser`, `username`, `first_name`, `last_name`, `email`, `is_staff`, `is_active`, `date_joined`, `profile_photo`) VALUES
+(1, '81dc9bdb52d04dc20036dbd8313ed055', NULL, 1, 'admin', 'Admin', 'User', 'admin@admin.com', 0, 1, '2019-11-27 02:07:14.000000', NULL);
 
 -- --------------------------------------------------------
 
@@ -76,6 +104,12 @@ ALTER TABLE `pages`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `superuser`
+--
+ALTER TABLE `superuser`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -86,10 +120,16 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT for table `superuser`
+--
+ALTER TABLE `superuser`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
