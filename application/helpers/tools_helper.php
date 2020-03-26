59839c1ef698fc53ba85389eb5cabca4530b2165
Can Okay <canokay2014@gmail.com>
2020-03-26 23:02:35 +0300

    1.1.3 - tools_helper update.

--- application/helpers/tools_helper.php
+++ application/helpers/tools_helper.php
@@ -6,12 +6,43 @@ function get_active_user(){
 
     $user = $t->session->userdata("user");
 
-    if($user)
+    if($user && $user->is_active == 1)
         return $user;
     else
         return false;
 
 }
+
+function get_notactive_user(){
+
+    $t = &get_instance();
+
+    $user = $t->session->userdata("user");
+
+    if($user && $user->is_active == 0)
+        return $user;
+    else
+        return false;
+
+}
+
+
+function get_superuser_user(){
+
+    $t = &get_instance();
+
+    $user = $t->session->userdata("user");
+
+    if($user && $user->is_active == 1 && $user->is_superuser == 1)
+        return $user;
+    else
+        return false;
+
+}
+
+
+
+
 
 function AutoSlugField($text){
     $turkce  = array("ç", "Ç", "ğ", "Ğ", "ü", "Ü", "ö", "Ö", "ı", "İ", "ş", "Ş", ".", ",", "!", "'", "\"", " ", "?", "*", "_", "|", "=", "(", ")", "[", "]", "{", "}");
