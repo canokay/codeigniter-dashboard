edc91973b23fedc4fed595a6e582b176fd8b655d
Can Okay <canokay2014@gmail.com>
2020-03-26 23:09:12 +0300

    1.1.9 - Topbar Gravatar added.

--- application/views/includes/dashboard/base/topbar.php
+++ application/views/includes/dashboard/base/topbar.php
@@ -24,7 +24,7 @@
             <li class="nav-item dropdown no-arrow d-sm-none">
               <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                 <i class="fas fa-search fa-fw"></i>
-              </a>
+              </a> 
               <!-- Dropdown - Messages -->
               <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
                 <form class="form-inline mr-auto w-100 navbar-search">
@@ -149,9 +149,19 @@
 
             <!-- Nav Item - User Information -->
             <li class="nav-item dropdown no-arrow">
-              <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Valerie Luna</span>
-                <img class="img-profile rounded-circle" src="https://source.unsplash.com/QAB-WJcbgJk/60x60">
+            <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								<span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php if(isset($user)){echo $user->username;}?></span>
+								<?php 
+								if(isset($user)){
+									echo "<img class='img-profile rounded-circle' src='https://www.gravatar.com/avatar/" , md5($user->email) , "?size=60'>";
+									}
+								else{
+									echo "<img class='img-profile rounded-circle' src='https://source.unsplash.com/QAB-WJcbgJk/60x60'>";
+								}									
+									
+									?>
+
+                
               </a>
               <!-- Dropdown - User Information -->
               <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
@@ -168,7 +178,7 @@
                   Activity Log
                 </a>
                 <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="<?php echo base_url("logout")?>">
+                <a class="dropdown-item" href="<?php echo base_url("sp-logout")?>">
                   <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                   Logout
                 </a>
