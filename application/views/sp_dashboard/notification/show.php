ab466f4675c2869c0ac029aa3c26a6f85ce7d512
Can Okay <canokay2014@gmail.com>
2020-08-27 06:41:56 +0300

    1.5.34 - render_card_header() and render_card_header_and_button() helpers added.

--- application/views/sp_dashboard/notification/show.php
+++ application/views/sp_dashboard/notification/show.php
@@ -1,6 +1,7 @@
 <div class="col-xl-12 col-lg-12">
     <div class="card shadow mb-4">
 		<!-- Card Header - Dropdown -->
+		<?php render_card_header_and_button($card_title);?>
 		<div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
             <h6 class="m-0 font-weight-bold text-primary"><?php echo $card_title;?></h6>
             <div class="dropdown no-arrow">
