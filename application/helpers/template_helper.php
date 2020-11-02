8631d9db5db47b85a876489b5985bbdcef6f1659
Can Okay <canokay2014@gmail.com>
2020-11-02 14:07:26 +0300

    1.6.1 - new template helpers added.

--- application/helpers/template_helper.php
+++ application/helpers/template_helper.php
@@ -21,6 +21,84 @@ function render_dashboard_view($context){
     return $t->load->view($t->project . "/layout",$context);
 }
 
+function render_dashboard_index_view($context){
+    $t = &get_instance();
+    $context['project'] =  $t->project;
+    $context['category'] =  $t->category;
+    $context['view'] =  "index";
+    $context['title'] =  $t->verbose_name_plural;
+    $context['card_title'] =  $t->verbose_name . " Listesi";
+    $context['DataTablesField'] =  "datatable";
+    $context['page_title_add_button'] =  1;
+    $context['user'] 					=	$t->user;
+    $context['verbose_name'] =  $t->verbose_name;
+    $context['verbose_name_plural'] =  $t->verbose_name_plural;
+    $context['notification_alerts'] 	=	$t->notification_alerts;
+    $context['ticket_alerts'] 		=	$t->ticket_alerts;
+    return $t->load->view($t->project . "/layout",$context);
+}
+
+function render_dashboard_create_view($context){
+    $t = &get_instance();
+    $context['project'] =  $t->project;
+    $context['category'] =  $t->category;
+    $context['view'] =  "create";
+    $context['title']		=	$t->verbose_name . " Oluştur";
+    $context['card_title']	=	$t->verbose_name . " Ekle";
+    $context['user'] 					=	$t->user;
+    $context['verbose_name'] =  $t->verbose_name;
+    $context['verbose_name_plural'] =  $t->verbose_name_plural;
+    $context['notification_alerts'] 	=	$t->notification_alerts;
+    $context['ticket_alerts'] 		=	$t->ticket_alerts;
+    return $t->load->view($t->project . "/layout",$context);
+}
+
+function render_dashboard_edit_view($context,$title){
+    $t = &get_instance();
+    $context['project'] =  $t->project;
+    $context['category'] =  $t->category;
+    $context['view'] =  "edit";
+    $context['user'] 					=	$t->user;
+    $context['title'] =  $title . " - Düzenle";
+    $context['card_title'] =  $title . " Güncelle";
+    $context['verbose_name'] =  $t->verbose_name;
+    $context['verbose_name_plural'] =  $t->verbose_name_plural;
+    $context['notification_alerts'] 	=	$t->notification_alerts;
+    $context['ticket_alerts'] 		=	$t->ticket_alerts;
+    return $t->load->view($t->project . "/layout",$context);
+}
+
+function render_dashboard_show_view($context,$title){
+    $t = &get_instance();
+    $context['project'] =  $t->project;
+    $context['category'] =  $t->category;
+    $context['view'] =  "show";
+    $context['title'] =  $title;
+    $context['card_title'] =  $title;
+    $context['user'] 					=	$t->user;
+    $context['verbose_name'] =  $t->verbose_name;
+    $context['verbose_name_plural'] =  $t->verbose_name_plural;
+    $context['notification_alerts'] 	=	$t->notification_alerts;
+    $context['ticket_alerts'] 		=	$t->ticket_alerts;
+    return $t->load->view($t->project . "/layout",$context);
+}
+
+function render_dashboard_delete_view($context,$title){
+    $t = &get_instance();
+    $context['project'] =  $t->project;
+    $context['category'] =  $t->category;
+    $context['view'] =  "delete";
+    $context['user'] 					=	$t->user;
+    $context['title'] =  $title . " - Sil";
+    $context['card_title'] =  $title . " - Silinecek";
+    $context['verbose_name'] =  $t->verbose_name;
+    $context['verbose_name_plural'] =  $t->verbose_name_plural;
+    $context['notification_alerts'] 	=	$t->notification_alerts;
+    $context['ticket_alerts'] 		=	$t->ticket_alerts;
+    return $t->load->view($t->project . "/layout",$context);
+}
+
+
 function render_card_header($card_header_title){
     echo "    <!-- Card Header - Dropdown -->";
     echo "    <div class='card-header py-3 d-flex flex-row align-items-center justify-content-between'>";
